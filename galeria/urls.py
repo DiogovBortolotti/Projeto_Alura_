21d5cd9839745ce6c72a284acd02e9aeacfd02c0
Diogo Vieira <diogovbortolotti@gmail.com>
2023-01-13 16:35:05 -0300

    Finalizado 1 Modulo

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from galeria.views import image, index
 
 urlpatterns = [
-    path('', index),
-    path('imagem/',  image),
+    path('', index, name='index'),
+    path('imagem/',  image, name='imagem'),
 ]
